1d3a85e7749a3d7a5b20cfae3c31438a743f3022
Thomas <thomas.crull6904@gmail.com>
2026-01-10 15:36:10 +0100

    Overhaul: changed relations between data formats

--- shared/model.go
+++ shared/model.go
@@ -1,15 +1,15 @@
 package shared
 
 // UserInfo holds information about a user of the ledger.
-// MSPID - the MSP ID of the user.
+// MSPID - the MSP id of the user.
 // SerialNumber - the serial number of the user.
 // CommonName - the common name of the user.
 // OrganizationalUnit - the organisational unit of the user. A set of roles and memberships within the organisation.
 type UserInfo struct {
-	MSPID              string   `json:"mspId"`
-	SerialNumber       string   `json:"serialNumber"`
-	CommonName         string   `json:"commonName"`
-	OrganizationalUnit []string `json:"organizationalUnit"`
+	MSPID              string   `json:"msp_id"`
+	SerialNumber       string   `json:"serial_number"`
+	CommonName         string   `json:"common_name"`
+	OrganizationalUnit []string `json:"organizational_unit"`
 }
 
 // Participant holds participant's information.
@@ -17,19 +17,27 @@ type UserInfo struct {
 // EncapsulatedKey - the participant's encapsulated key.
 // HomomorphicSharedKeyCypher - the homomorphic shared key cypher. This key can be retrieved using the decapsulated key, and it is used to encrypt the model update.
 // CommunicationKeyCypher - the participant's communication key cypher. This key is used to encrypt the messages exchanged between the participant and the aggregator.
+// MSPID - the MSP id of the user who created the participant.
+// SerialNumber - the serial number of the user who created the participant.
 type Participant struct {
-	ParticipantId              string `json:"participant_id"`
+	ParticipantId              int    `json:"participant_id"`
 	EncapsulatedKey            string `json:"encap_key"`              // STEP 2
 	HomomorphicSharedKeyCypher string `json:"homomorphic_key_cypher"` //STEP 4
 	CommunicationKeyCypher     string `json:"comm_key_cypher"`
+	MSPID                      string `json:"msp_id"`
+	SerialNumber               string `json:"serial_number"`
 }
 
 // Aggregator holds aggregator's information.
 // AggregatorId - the aggregator's id.
 // CommunicationKeysCyphers - a map of communication key cyphers, where the key is the participant's id. These keys are used to encrypt the messages exchanged between the aggregator and the participant.
+// MSPID - the MSP id of the user who created the aggregator.
+// SerialNumber - the serial number of the user who created the aggregator.
 type Aggregator struct {
-	AggregatorId             string            `json:"aggregator_id"`
+	AggregatorId             int               `json:"aggregator_id"`
 	CommunicationKeysCyphers map[string]string `json:"comm_keys_cyphers"`
+	MSPID                    string            `json:"msp_id"`
+	SerialNumber             string            `json:"serial_number"`
 }
 
 // ParticipantModelMetadata represents a participant's model update's metadata.
@@ -39,7 +47,7 @@ type Aggregator struct {
 // HomomorphicHash - the homomorphic hash of the model update.
 type ParticipantModelMetadata struct {
 	Epoch           int    `json:"epoch"`
-	ParticipantId   string `json:"participant_id"`
+	ParticipantId   int    `json:"participant_id"`
 	ModelHashCid    string `json:"model_hash_cid"`
 	HomomorphicHash string `json:"homomorphic_hash"`
 }
@@ -50,22 +58,22 @@ type ParticipantModelMetadata struct {
 // ParticipantIds - the participants' ids that contributed to the global model update.
 // ModelHashCid - the IPFS CID of the global model update.
 type AggregatorModelMetadata struct {
-	Epoch          int      `json:"epoch"`
-	AggregatorId   string   `json:"aggregator_id"`
-	ParticipantIds []string `json:"participant_ids"`
-	ModelHashCid   string   `json:"model_hash_cid"`
+	Epoch          int    `json:"epoch"`
+	AggregatorId   int    `json:"aggregator_id"`
+	ParticipantIds []int  `json:"participant_ids"`
+	ModelHashCid   string `json:"model_hash_cid"`
 }
 
 // LogEntry holds a transaction log entry.
-// TxID - the transaction id.
+// TxId - the transaction id.
 // TxCreator - information about the creator of the transaction.
 // Timestamp - the timestamp of the transaction.
 // IsDelete - whether the entry is a delete operation.
 // Changes - the changes made by the transaction.
 type LogEntry struct {
-	TxID      string      `json:"txId"`
-	TxCreator UserInfo    `json:"txCreator"`
+	TxId      string      `json:"tx_id"`
+	TxCreator UserInfo    `json:"tx_creator"`
 	Timestamp string      `json:"timestamp"`
-	IsDelete  bool        `json:"isDelete"`
-	Changes   interface{} `json:"value"`
+	IsDelete  bool        `json:"is_delete"`
+	Changes   interface{} `json:"changes"`
 }
